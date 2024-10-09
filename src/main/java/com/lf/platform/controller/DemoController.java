aedfe9fd748611df2a21cecdd93f34cfee1aba5b
srchen1987 <suxuan696@gmail.com>
2024-10-09 09:14:53 +0800

    update

--- src/main/java/com/lf/platform/controller/DemoController.java
+++ src/main/java/com/lf/platform/controller/DemoController.java
@@ -10,43 +10,17 @@ import com.anywide.dawdler.clientplug.web.annotation.ResponseBody;
 import com.anywide.dawdler.core.result.BaseResult;
 import com.anywide.dawdler.jedis.JedisOperator;
 import com.anywide.dawdler.jedis.annotation.JedisInjector;
-import com.anywide.dawdler.rabbitmq.consumer.Message;
-import com.anywide.dawdler.rabbitmq.consumer.annotation.RabbitListener;
-import com.anywide.dawdler.rabbitmq.provider.RabbitProvider;
-import com.anywide.dawdler.rabbitmq.provider.annotation.RabbitInjector;
 
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
 	Logger logger = LoggerFactory.getLogger(DemoController.class);
 	
-	@RabbitInjector("rabbitmq")
-	private RabbitProvider rabbitProvider;
+	// @RabbitInjector("rabbitmq")
+	// private RabbitProvider rabbitProvider;
 
 	@JedisInjector("redis")
 	private JedisOperator JedisOperator;
-
-    @ResponseBody
-    @RequestMapping(value="/send", method = RequestMethod.GET)
-    public BaseResult<Void> send(String message) throws Exception {
-		System.out.println("send:"+message);
-		logger.info("send:"+message);
-		rabbitProvider.publish("", "testQueue", null, message.getBytes()); 
-		return new BaseResult<>(true);
-    }
-
-	@RabbitListener(fileName = "rabbitmq",queueName = "testQueue")
-    public BaseResult<Void> receive(Message message) {
-		String messageBody = new String(message.getBody());
-		System.out.println("receive:"+messageBody);
-		if(messageBody.equals("error")){
-			logger.error("receive:"+messageBody);
-		}else{
-			logger.info("receive:"+messageBody);
-		}
-		
-		return new BaseResult<>(true);
-    }
 
 
 	@ResponseBody
